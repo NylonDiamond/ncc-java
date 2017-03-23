b14637c1bd011d3f719fb41d010224455bb060e3
Jesse Skelton <jskelton8@oreillyauto.com>
2017-03-23 17:08:10 -0500

    gui set up

--- src/main/java/sf/codingcomp/gui/firstGuiForm.java
+++ src/main/java/sf/codingcomp/gui/firstGuiForm.java
@@ -1,8 +1,14 @@
 package sf.codingcomp.gui;
 
+import sf.codingcomp.SearchServiceImplementation;
+import sf.codingcomp.model.Entertainment;
+import sf.codingcomp.model.Game;
+import sf.codingcomp.model.Movie;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 /**
  * Created by jskelton8 on 3/23/2017.
@@ -10,12 +16,52 @@ import java.awt.event.ActionListener;
 public class firstGuiForm {
     private JButton submitButton;
     private JPanel firstPanel;
+    public JTextField textField1;
+    private JButton searchMoviesButton;
+    private JButton searchGamesButton;
 
     public firstGuiForm() {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                JOptionPane.showMessageDialog(null, "My first form message!");
+                String title =  textField1.getText();
+                SearchServiceImplementation service = new SearchServiceImplementation();
+                List<Entertainment> items = service.searchGameAndMovies(title);
+    System.out.print(items);
+                String concat = "";
+                for(Entertainment item : items){
+                    concat += item.getTitle() + '\n';
+                }
+                JOptionPane.showMessageDialog(null, concat);
+
+            }
+        });
+        searchMoviesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String title =  textField1.getText();
+                SearchServiceImplementation service = new SearchServiceImplementation();
+                List<Movie> items = service.searchMovie(title);
+                System.out.print(items);
+                String concat = "";
+                for(Entertainment item : items){
+                    concat += item.getTitle() + '\n';
+                }
+                JOptionPane.showMessageDialog(null, concat);
+            }
+        });
+        searchGamesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String title =  textField1.getText();
+                SearchServiceImplementation service = new SearchServiceImplementation();
+                List<Game> items = service.searchGame(title);
+                System.out.print(items);
+                String concat = "";
+                for(Entertainment item : items){
+                    concat += item.getTitle() + '\n';
+                }
+                JOptionPane.showMessageDialog(null, concat);
             }
         });
     }
